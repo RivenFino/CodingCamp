3868178db3c2a83f5d2c1f59e48bb7a863cb6422
RivenFino <btrcatdf07a5@gmail.com>
2025-01-21 22:54:46 +0800

    add submission folder

---
+++ About Yorushika/assets/scripts/getURL.js
@@ -0,0 +1,30 @@
+// Ambil parameter album dari URL
+const urlParams = new URLSearchParams(window.location.search);
+const album = urlParams.get('album');
+const menuItems = document.querySelectorAll('.menu');
+
+// Sembunyikan semua album
+document.querySelectorAll('.album-item').forEach((albumItem) => {
+    albumItem.style.display = 'none';
+});
+
+if (album) {
+    const targetAlbum = document.getElementById(album);
+    if (targetAlbum) {
+        targetAlbum.style.display = 'flex';
+        // Scroll ke album yang dipilih
+        targetAlbum.scrollIntoView({
+            behavior: 'instant', 
+            block: 'center'       
+        });
+    }
+}
+
+menuItems.forEach((item) => {
+    item.addEventListener('click', function(event) {
+        // Menghapus query string jika ada
+        if (window.location.search) {
+            history.pushState({}, document.title, window.location.pathname);
+        }
+    });
+});
